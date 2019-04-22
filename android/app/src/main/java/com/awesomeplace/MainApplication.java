e447afa3c8b13764218e1e2c421b83dd3cde03e6
Hamza Yousaf <Hamzayousaf933@gmail.com>
2019-04-22 23:59:11 +0500

    google map done

--- android/app/src/main/java/com/awesomeplace/MainApplication.java
+++ android/app/src/main/java/com/awesomeplace/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.reactnativenavigation.NavigationApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -59,7 +60,8 @@ public class MainApplication extends NavigationApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           //new MainReactPackage(),
-          new VectorIconsPackage()
+          new VectorIconsPackage(),
+          new MapsPackage()
       );
     }
 
