7c7135c076dcce2fba1904fd2b7362762f69e6c6
Hamza Yousaf <Hamzayousaf933@gmail.com>
2019-04-25 23:41:14 +0500

    splash screen done

--- android/app/src/main/java/com/awesomeplace/MainActivity.java
+++ android/app/src/main/java/com/awesomeplace/MainActivity.java
@@ -1,5 +1,3 @@
-package com.awesomeplace;
-
 // import com.facebook.react.ReactActivity;
 
 // public class MainActivity extends ReactActivity {
@@ -14,9 +12,29 @@ package com.awesomeplace;
 //     }
 // }
 
+package com.awesomeplace;
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.util.TypedValue;
 
 import com.reactnativenavigation.controllers.SplashActivity;
 
-public class MainActivity extends SplashActivity {
-    
+public class MainActivity extends SplashActivity  {
+    @Override
+    public LinearLayout createSplashLayout() {
+      LinearLayout view = new LinearLayout(this);
+      TextView textView = new TextView(this);
+
+      view.setBackgroundColor(Color.parseColor("#521751"));
+      view.setGravity(Gravity.CENTER);
+
+      textView.setTextColor(Color.parseColor("#fa923f"));
+      textView.setText("Awesome Places");
+      textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
+
+      view.addView(textView);
+      return view;
+    }
 }
